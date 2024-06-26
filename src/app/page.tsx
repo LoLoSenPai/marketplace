c8eda5f9ea5c64c5fbbe18921dc6e118a3b374f1
LoLoSenPai <loic.dlugosz@gmail.com>
2024-06-26 17:45:54 +0200

    Update page to use video instead of image for hero asset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
-import Link from "next/link";
 import Image from "next/image";
 import { InfiniteMovingCards } from "@/components/InfiniteMovingCards";
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <div className="pb-10 space-y-12">
 
       <div className="flex justify-center p-2">
-        <Image
-          src="/opening.avif"
+        <video
+          src="/ogrehunt-intro.webm"
           width={1920}
-          height={640}
-          alt="Hero asset, NFT marketplace"
-          quality={100}
-          className="mb-4 max-w-screen"
-        />
+          height={1080}
+          autoPlay
+          loop
+          muted
+          className="h-auto mb-4 pointer-events-none max-w-screen"
+        >
+          Your browser does not support the video tag.
+        </video>
       </div>
 
       <div className='p-6 text-xl text-center card rounded-xl'>
